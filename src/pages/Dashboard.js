e66bc730fb8b83b3c594e75ec983a2dffbf0e454
Alexander Harris <alexharris@alexanders-mbp.attlocal.net>
2021-10-31 17:50:23 -0400

    adds pages

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,8 @@
+const Dashboard = (props) => {
+    return (
+        <main>
+            <h1>Dashboard</h1>
+        </main>
+    )
+};
+export default Dashboard;
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+const Home = (props) => {
+    return (
+        <main>
+            <h1>Home</h1>
+        </main>
+    )
+};
+export default Home;
