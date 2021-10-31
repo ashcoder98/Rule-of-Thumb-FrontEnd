0fc5601b5e30efe101deaba73424683984df519c
Alexander Harris <alexharris@alexanders-mbp.attlocal.net>
2021-10-31 17:29:12 -0400

    setting up firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCRGX8CZ50RVBpYa5Q6zQ82frhj5fDtjIU",
+    authDomain: "rule-of-thumb-31aa1.firebaseapp.com",
+    projectId: "rule-of-thumb-31aa1",
+    storageBucket: "rule-of-thumb-31aa1.appspot.com",
+    messagingSenderId: "1009741313431",
+    appId: "1:1009741313431:web:6f074b63be561905b700d2"
+  };
+
+  firebase.initializeApp(firebaseConfig);
