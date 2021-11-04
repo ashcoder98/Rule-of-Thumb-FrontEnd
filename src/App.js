6b59158fbca266cdb322e1342265647a4b4a2b07
Alexander Harris <alexharris@alexanders-mbp.attlocal.net>
2021-11-04 18:52:58 -0400

    comments out authentication for now

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import Header from './components/Header.js';
 import Main from "./components/Main.js";
 import './App.css';
 
-import { useState, useEffect } from 'react'
-import { auth } from './services/firebase';
+// import { useState, useEffect } from 'react'
+// import { auth } from './services/firebase';
 
 
 function App() {
-  const [ user, setUser ] = useState(null);
-  useEffect(() => {
-   const unsubscribe =  auth.onAuthStateChanged(user => setUser(user));
-   return () => unsubscribe();
-  }, [])
+  // const [ user, setUser ] = useState(null);
+  // useEffect(() => {
+  //  const unsubscribe =  auth.onAuthStateChanged(user => setUser(user));
+  //  return () => unsubscribe();
+  // }, [])
 
 
       return (
      <>
-      <Header user={user}/>
+      <Header
+      //  user={user}
+       />
       <Main />
 
      </>
